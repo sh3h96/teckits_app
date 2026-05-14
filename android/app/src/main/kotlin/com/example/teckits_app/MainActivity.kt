3f41c170870d66ec04d8c7882c7e0fba64937cdf
Shehab Shawqi Abduh Al-Dhahra <Shehabshawgi@gmail.com>
2026-05-14 21:09:59 +0300

    Initial commit with professional README

---
+++ android/app/src/main/kotlin/com/example/teckits_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.teckits_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Color.fromARGB(255, 74, 124, 112),
+      body: Container( ),
+  ),
+  ),
+  );
+}
